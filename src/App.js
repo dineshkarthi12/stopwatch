07b9913ff16b21837654b1937387e301229343e6
dineshkarthi12 <120086241+dineshkarthi12@users.noreply.github.com>
2023-03-19 11:19:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StopWatch from './StopWatch';
+
+function App() {
+  return (
+    <div className="App">
+     <StopWatch />
+    </div>
+  );
+}
+
+export default App;
